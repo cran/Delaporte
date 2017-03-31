77d8370dafb252438883e323914513707c370503
Avraham Adler <Avraham.Adler@gmail.com>
2017-03-31 11:48:41 +0000

    version 6.0.0

--- R/Delaporte.R
+++ R/Delaporte.R
@@ -6,7 +6,8 @@ ddelap <- function(x, alpha, beta, lambda, log = FALSE){
   if(any(x > floor(x))) {
     warning("Non-integers passed to ddelap. These will have 0 probability.")
   }
-  .Call(ddelap_C, x, alpha, beta, lambda, log)
+  if(log) log_f <- 1L else log_f <- 0L
+  .Call(ddelap_C, x, alpha, beta, lambda, log_f)
 }
 
 pdelap <- function(q, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE){
@@ -14,20 +15,26 @@ pdelap <- function(q, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE){
   if(!is.double(alpha)) {storage.mode(alpha) <- 'double'}
   if(!is.double(beta)) {storage.mode(beta) <- 'double'}
   if(!is.double(lambda)) {storage.mode(lambda) <- 'double'}
-  .Call(pdelap_C, q, alpha, beta, lambda, lower.tail, log.p)
+  if(lower.tail) lt_f <- 1L else lt_f <- 0L
+  if(log.p) lp_f <- 1L else lp_f <- 0L
+  .Call(pdelap_C, q, alpha, beta, lambda, lt_f, lp_f)
 }
 
-qdelap <- function(p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE, exact = TRUE, old = FALSE){
+qdelap <- function(p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE, exact = TRUE){
   if(!is.double(p)) {storage.mode(p) <- 'double'}
   if(!is.double(alpha)) {storage.mode(alpha) <- 'double'}
   if(!is.double(beta)) {storage.mode(beta) <- 'double'}
   if(!is.double(lambda)) {storage.mode(lambda) <- 'double'}
   QDLAP <- double(length(p))
   if (exact) {
-    QDLAP <- .Call(qdelap_C, p, alpha, beta, lambda, lower.tail, log.p)
+    if(lower.tail) lt_f <- 1L else lt_f <- 0L
+    if(log.p) lp_f <- 1L else lp_f <- 0L
+    QDLAP <- .Call(qdelap_C, p, alpha, beta, lambda, lt_f, lp_f)
   } else {
     if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('Parameters must be strictly greater than 0. Please use exact version, if necessary, to prevent spurious results')
+      stop('Parameters must be strictly greater than 0. Please use exact version, if necessary, to prevent spurious results.')
+    if(length(alpha) > 1 || length(beta) > 1 || length(lambda) > 1)
+      stop('Quantile approximation relies on pooling and thus is not accurate when passing vector-valued parameters. Please use exact version.')
     if (log.p) p <- exp(p)
     if (!lower.tail) p <- 1 - p
     pValid <- p[p > 0 & p < 1]
@@ -35,12 +42,8 @@ qdelap <- function(p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE, exa
     p0 <- p[p == 0]
     pInf <- p[p >= 1]
     n <- min(10 ^ (ceiling(log(alpha * beta + lambda, 10)) + 5), 1e7)
-    if (old) {
-      .Defunct(msg = 'This option is defunct. Use old = FALSE. The "old" option may be removed at any time and exact = FALSE will default to the new method.')
-    } else {
-      ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
-      DP <- rpois(n, lambda = (ShiftedGammas + lambda))
-    }
+    ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
+    DP <- rpois(n, lambda = (ShiftedGammas + lambda))
     QValid <- as.vector(quantile(DP, pValid, na.rm = TRUE, type = 8))
     QNan <- rep.int(NaN, times = length(pNan))
     Q0 <- rep.int(0, times = length(p0))
@@ -50,7 +53,7 @@ qdelap <- function(p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE, exa
   return(QDLAP)
 }
 
-rdelap <- function(n, alpha, beta, lambda, exact = TRUE, old = FALSE){
+rdelap <- function(n, alpha, beta, lambda, exact = TRUE){
   if(!is.integer(n)) {storage.mode(n) <- 'integer'}
   if(!is.double(alpha)) {storage.mode(alpha) <- 'double'}
   if(!is.double(beta)) {storage.mode(beta) <- 'double'}
@@ -58,13 +61,9 @@ rdelap <- function(n, alpha, beta, lambda, exact = TRUE, old = FALSE){
   RDLAP <- double(length(n))
   if (!exact) {
     if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('Parameters must be strictly greater than 0. Please use exact version, if necessary, to prevent spurious results')
-    if (old) {
-      .Defunct(msg = 'This option is defunct. Use old = FALSE. The "old" option may be removed at any time and exact = FALSE will default to the new method.')
-    } else {
-      ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
-      RDLAP <- rpois(n, lambda = (ShiftedGammas + lambda))
-    }
+      stop('Parameters must be strictly greater than 0. Please use exact version, if necessary, to prevent spurious results.')
+    ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
+    RDLAP <- rpois(n, lambda = (ShiftedGammas + lambda))
   } else {
     RDLAP <- .Call(rdelap_C, n, alpha, beta, lambda)
   }
