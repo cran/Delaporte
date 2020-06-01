646343dc6d585c0b308adbbd2aea70201b31e054
Avraham Adler <Avraham.Adler@gmail.com>
2020-06-01 06:40:08 +0000

    version 7.0.3

---
+++ tests/testthat/test-package.r
@@ -0,0 +1,14 @@
+context("Package Maintenance")
+test_that("Version", {
+  expect_match(toBibtex(citation('Delaporte')),
+               as.character(packageVersion('Delaporte')),
+               fixed = TRUE, all = FALSE)
+  # expect_match(scan("../../README.md", what = 'character', skip = 23L,
+  #                   nlines = 1L, quiet = TRUE),
+  #              as.character(packageVersion('Delaporte')),
+  #              fixed = TRUE, all = FALSE)
+  # expect_match(scan("../../README.md", what = 'character', skip = 33L,
+  #                   nlines = 1L, quiet = TRUE),
+  #              as.character(packageVersion('Delaporte')),
+  #              fixed = TRUE, all = FALSE)
+})
